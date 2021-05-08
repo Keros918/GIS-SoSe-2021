1a4c0b69a58dd908698f14b7edc2350ce9b904c3
Keros918 <haasleon.s@gmx.de>
2021-05-08 04:14:15 +0200

    Aufgabe2.3_anfang

---
+++ Kapitel_2/Aufgabe_3/script.js
@@ -0,0 +1,57 @@
+"use strict";
+var Aufgabe3;
+(function (Aufgabe3) {
+    // Aufgabe 1
+    class Rechteck {
+        constructor(_classname) {
+            this.rechteck = document.createElement("div");
+            this.random = Math.random() * 10;
+            this.posX = this.random * 150;
+            this.posY = this.random * 60;
+            this.width = Math.random() * 200;
+            this.height = Math.random() * 200;
+            this.classname = _classname;
+        }
+        buildRect() {
+            this.rechteck.style.position = "absolute";
+            this.rechteck.style.width = this.width + "px";
+            this.rechteck.style.height = this.height + "px";
+            this.rechteck.style.top = this.posY + "px";
+            this.rechteck.style.left = this.posX + "px";
+            this.rechteck.style.borderStyle = "solid";
+            this.rechteck.className = this.classname;
+            document.body.appendChild(this.rechteck);
+        }
+    }
+    let anzahl = Math.random() * 10;
+    anzahl = Math.round(anzahl);
+    let rectangles = [];
+    while (anzahl > 0) {
+        rectangles.push(new Rechteck("rect"));
+        rectangles[rectangles.length - 1].buildRect();
+        anzahl--;
+    }
+    let addRect = document.createElement("button");
+    let reset = document.createElement("button");
+    addRect.style.width = "100px";
+    addRect.style.height = "50px";
+    reset.style.width = "100px";
+    reset.style.height = "50px";
+    addRect.innerText = "Rechteck hinzufügen";
+    reset.innerText = "Reset";
+    document.body.appendChild(addRect);
+    document.body.appendChild(reset);
+    addRect.addEventListener("click", handleRect);
+    reset.addEventListener("click", handleReset);
+    function handleRect() {
+        let rect = new Rechteck("rectNeu");
+        rect.buildRect();
+    }
+    function handleReset() {
+        let remove = document.getElementsByClassName("rectNeu");
+        while (remove.length > 0) {
+            remove.item(0).remove();
+        }
+    }
+})(Aufgabe3 || (Aufgabe3 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Kapitel_2/Aufgabe_3/script.ts
@@ -0,0 +1,67 @@
+namespace Aufgabe3 {
+    // Aufgabe 1
+    class Rechteck {
+        rechteck: HTMLDivElement;
+        random: number;
+        posX: number;
+        posY: number;
+        width: number;
+        height: number;
+        classname: string;
+
+        constructor(_classname: string) {
+            this.rechteck = document.createElement("div");
+            this.random = Math.random() * 10;
+            this.posX = this.random * 150;
+            this.posY = this.random * 60;
+            this.width = Math.random() * 200;
+            this.height = Math.random() * 200;
+            this.classname = _classname;
+        }
+
+        buildRect(): void {
+            this.rechteck.style.position = "absolute";
+            this.rechteck.style.width = this.width + "px";
+            this.rechteck.style.height = this.height + "px";
+            this.rechteck.style.top = this.posY + "px";
+            this.rechteck.style.left = this.posX + "px";
+            this.rechteck.style.borderStyle = "solid";
+            this.rechteck.className = this.classname;
+            document.body.appendChild(this.rechteck);
+        }
+    }
+
+    let anzahl: number = Math.random() * 10;
+    anzahl = Math.round(anzahl);
+    let rectangles: Rechteck[] = [];
+    while (anzahl > 0) {
+        rectangles.push(new Rechteck("rect"));
+        rectangles[rectangles.length - 1].buildRect();
+        anzahl--;
+    }
+
+    let addRect: HTMLButtonElement = document.createElement("button");
+    let reset: HTMLButtonElement = document.createElement("button");
+    addRect.style.width = "100px";
+    addRect.style.height = "50px";
+    reset.style.width = "100px";
+    reset.style.height = "50px";
+    addRect.innerText = "Rechteck hinzufügen";
+    reset.innerText = "Reset";
+    document.body.appendChild(addRect);
+    document.body.appendChild(reset);
+    addRect.addEventListener("click", handleRect);
+    reset.addEventListener("click", handleReset);
+
+    function handleRect(): void {
+        let rect: Rechteck = new Rechteck("rectNeu");
+        rect.buildRect();
+    }
+
+    function handleReset(): void {
+        let remove: HTMLCollection = document.getElementsByClassName("rectNeu");
+        while (remove.length > 0) {
+            remove.item(0).remove();
+        }
+    }
+}
